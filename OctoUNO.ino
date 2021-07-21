9081e696a20b1b7da666a8c2d2b7229f6b666072
Forrest Erickson <forresterickson@comcast.net>
2021-07-21 18:17:24 -0400

    Sweeping voltage and measureing current. Report out serial port.

--- OctoUNO.ino
+++ OctoUNO.ino
@@ -5,7 +5,7 @@
 
    Author: Forrest L Erickson
    Date: 20210720  Happy Moon Landing Annaversary
-   Lisense: Dedicated to the public domain.
+   License: Dedicated to the public domain.
    Free to use. This program is designed to kill you but not guarenteed to do so.
    Date: 20210721 Rework circuits per note book Start 2013 Page 79.
 */
@@ -16,29 +16,36 @@
 
     For the OcotUNO This will be done with done using one PWM output filtered and double buffered to make Vtest.
     The DUT will be tested by connecting from the Vtest into a theven source at Vcc/2 with Req =10K/2
-    
+
    Pinout
    Vtest from WPM on Pin 5.
    A0 to ICPin under test at Vtest
    A1 to ICPin under test at Vthev
 
+   The current in the DUT is about 1uA per setup size from 1023/2
+
    RESULES: The ramp voltage is from 1 to about 4.6 Volts. Ramp time is about 250mS.
 */
 
-int VccTest = 5;
+int VccTest = 5;    //Use PWM output 5, 980Hz.
+const int VCC = 5;  //Volts
+const int REQ = 5000;   //REQ is the Rthevenin of 10K // 10K
 const int MAXPWM = 255;
 const int STEPSIZE = 1;
-int ii = 511; 
-  
+int ii = MAXPWM / 2; // Mid point
+int Vdut = 0;
+int Idut =0; 
+
+
 //Set LED for Uno or ESP32 Dev Kit on board blue LED.
 const int HIGH_TIME_LED = 900;
 const int LOW_TIME_LED = 100;
 long lastLEDtime = 0;
 long nextLEDchange = 100; //time in ms.
 
-void wink(){
-    //Wink the LED
-  if (((millis() - lastLEDtime) > nextLEDchange)||(millis()< lastLEDtime)) {
+void wink() {
+  //Wink the LED
+  if (((millis() - lastLEDtime) > nextLEDchange) || (millis() < lastLEDtime)) {
     if (digitalRead(LED_BUILTIN) == LOW) {
       digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
       nextLEDchange = HIGH_TIME_LED;
@@ -55,11 +62,11 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);      // set the LED pin mode
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level) Start of setup()
   Serial.begin(115200);
-  
+
   //Set PWM drive pins to output.
-  pinMode(VccTest, OUTPUT); 
+  pinMode(VccTest, OUTPUT);
   analogWrite(VccTest, 127);  //Set to mid point
-  digitalWrite(LED_BUILTIN, LOW);   // end of setup()  
+  digitalWrite(LED_BUILTIN, LOW);   // end of setup()
 }
 
 void loop() {
@@ -68,13 +75,24 @@ void loop() {
 
   //Curve Trace. Measure A0, A1
   //Calculate Vdut and Idut
+  int VdutPlus = analogRead(A0);
+  int VdutMinus = analogRead(A1);
+  Vdut = VdutPlus-VdutMinus;
+  Idut = (VdutMinus- 512);  //uAmps
+ 
+//  Serial.print(VdutPlus);
+  Serial.print(Vdut);
+  Serial.print(", ");
+  Serial.println(Idut);
+//  Serial.println(Idut-Vdut);
 
   //Ramp VccTest
-    Serial.println(ii);
-    analogWrite(VccTest, ii);
-    ii = ii + STEPSIZE;
-    if (ii > MAXPWM){
-      ii = 0;
-    }
-    delay(1);     //One millisecond
+  //Serial.println(ii);
+  analogWrite(VccTest, ii);
+  ii = ii + STEPSIZE;
+  if (ii > MAXPWM) {
+    ii = 0;
+  }
+  delay(1);     //One millisecond
+  
 }//Loop
